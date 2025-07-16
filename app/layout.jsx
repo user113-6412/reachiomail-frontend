091fe02fcd2411d9d8309551f1f73982925f0fc9
Victor Jarm <viktorasjarm92@gmail.com>
2025-07-16 22:41:59 +0700

    added google analytics gtag script

--- app/layout.jsx
+++ app/layout.jsx
@@ -16,41 +16,15 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        {/* GA4 */}
-        <script
-          async
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
-        />
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-W89LC3DKMZ"></script>
         <script
           dangerouslySetInnerHTML={{
             __html: `
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
-            `,
-          }}
-        />
-        
-        {/* Plausible */}
-        <script
-          defer
-          data-domain="mailmergegooglesheets.com"
-          src="https://plausible.io/js/plausible.js"
-        />
-        
-        {/* LogRocket */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(h,o,t,j,a,r){
-                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                h._hjSettings={hjid:${process.env.NEXT_PUBLIC_LOGROCKET_ID},hjsv:6};
-                a=o.getElementsByTagName('head')[0];
-                r=o.createElement('script');r.async=1;
-                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-                a.appendChild(r);
-              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+              gtag('config', 'G-W89LC3DKMZ');
             `,
           }}
         />
